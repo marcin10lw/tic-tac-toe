d375be7de399955f8076f9d22f018a9ca1874e1c
marcin10lw <marcin10lw@wp.pl>
2023-05-13 12:02:29 +0200

    created Game.tsx

---
+++ src/components/Game.tsx
@@ -0,0 +1,13 @@
+import Board from "./Board";
+
+const Game = () => {
+  const handleClick = () => {};
+
+  const jumpTo = () => {};
+
+  const renderMovies = () => {};
+
+  return <Board onClick={handleClick} />;
+};
+
+export default Game;
